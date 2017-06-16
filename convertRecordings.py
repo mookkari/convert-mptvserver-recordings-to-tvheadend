166bef4ad3f7d2f17cd27e625466c1a142c3cd4c
ksya <k.schasfoort@gmail.com>
2017-06-16 16:43:21 +0200

    Initial release

---
+++ convertRecordings.py
@@ -0,0 +1,124 @@
+import xml.etree.ElementTree as ET
+import sys
+import datetime, time
+import json
+import os
+import hashlib
+
+print(sys.version)
+
+baseDir = "\\\\10.8.3.1\\Opnames\\"
+baseServerDir = "/srv/ZFSDisk/Opnames/"
+baseTVHlogDir = "/home/hts/.hts/tvheadend/dvr/log"
+
+
+def get_filepaths(directory):
+    """
+    This function will generate the file names in a directory 
+    tree by walking the tree either top-down or bottom-up. For each 
+    directory in the tree rooted at directory top (including top itself), 
+    it yields a 3-tuple (dirpath, dirnames, filenames).
+    """
+    file_paths = []  # List which will store all of the full filepaths.
+
+    # Walk the tree.
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            _, file_extension = os.path.splitext(filename)
+            if file_extension == '.xml':
+                # Join the two strings in order to form the full filepath.
+                filepath = os.path.join(root, filename)
+                file_paths.append(filepath)  # Add it to the list.
+
+    return file_paths  # Self-explanatory.
+
+# Run the above function and store its results in a variable.   
+xmlFiles = get_filepaths(baseServerDir)
+
+
+
+for recXmlFile in xmlFiles:
+    print(recXmlFile)
+
+    recFile = recXmlFile.replace('xml', 'ts').replace('\\', '/')
+    print(recFile)
+
+
+    #read data from MediaPortal xml
+    tree = ET.parse(recXmlFile)
+    root = tree.getroot()
+    #print(root.findAll('SimpleTag'))
+    tags = tree.findall(".//SimpleTag")
+
+    data = {}
+    for i in tags: 
+        name = i.find('name').text
+        value = i.find('value').text
+
+        if name == 'STARTTIME' or name == 'ENDTIME':
+            value = int(time.mktime(datetime.datetime.strptime(value, '%Y-%m-%d %H:%M').timetuple()))
+        elif name == 'COMMENT':
+            value = value.split('. ')
+            subtitle = value[0]
+            del value[0]
+            description = '. '.join(value)
+            data['SUBTITLE'] = subtitle
+            data['DESCRIPTION'] = description
+        else:
+            value = value.replace('\n      ', '')
+        data[name] = value
+
+    #print(data)
+
+
+    #make json for tvheadend
+    jd = {}
+
+    jd["enabled"] = True
+    jd["start"] = data['STARTTIME']
+    jd["start_extra"] = 0
+    jd["stop"] = data['ENDTIME']
+    jd["stop_extra"] = 0
+    jd["channel"] = "3af7cb4eae5dfa8c2f0e03d0c641ef49"
+    jd["channelname"] = data['CHANNEL_NAME']
+    jd["title"] = {}
+    jd["title"]["dut"] = data['TITLE']
+    jd["subtitle"] = {}
+    jd["subtitle"]["dut"] = data['SUBTITLE']
+    jd["description"] = {}
+    jd["description"]["dut"] = data['DESCRIPTION']
+    jd["pri"] = 2
+    jd["retention"] = 0
+    jd["removal"] = 0
+    jd["playposition"] = 0
+    jd["playcount"] = 2
+    jd["config_name"] = "3d6a906a6b6ad5ca25867270db2f192e"
+    jd["owner"] = "admin"
+    jd["creator"] = "admin"
+    jd["errorcode"] = 0
+    jd["errors"] = 0
+    jd["data_errors"] = 0
+    jd["dvb_eid"] = 0
+    jd["noresched"] = True
+    jd["norerecord"] = False
+    jd["fileremoved"] = 0
+    jd["autorec"] = ""
+    jd["timerec"] = ""
+    jd["parent"] = ""
+    jd["child"] = ""
+    jd["content_type"] = 1
+    jd["broadcast"] = 0
+    jd["comment"] = "Imported from MediaPortal"
+    jd["files"] = [{}]
+    jd["files"][0]["filename"] = recFile
+    jd["files"][0]["start"] = data['STARTTIME']-300
+    jd["files"][0]["stop"] = data['ENDTIME']+300
+
+    jsonString = json.dumps(jd, sort_keys=True, indent="\t").encode('utf-8');
+    #print(jsonString)
+    md5name = hashlib.md5(jsonString).hexdigest()
+    print(md5name)
+    if not os.path.exists(baseTVHlogDir + '/MPimport/'):
+        os.mkdir(baseTVHlogDir + '/MPimport/')
+    with open(baseTVHlogDir + '/MPimport/' + md5name, 'w') as outfile:
+        json.dump(jd, outfile)
